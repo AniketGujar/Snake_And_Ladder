c2ec5780233c15120bbfbdb62a8649e0998aeef1
AniketGujar <gujaraniket7875@gmail.com>
2021-01-10 00:42:07 +0530

    Added: Code to play game with two players

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ console.log("Welcome to the Snake And Ladder Game..!!");
 let playerPosition = 0;
 const WINNING_POS = 100;
 let diceCount = 0;
+let player = "Player 1";
+let playerOnePosition = 0;
+let playerTwoPosition = 0;
 
 class SnakeAndLadder {
 
@@ -14,12 +17,13 @@ class SnakeAndLadder {
     }
 
     option = () => {
+        let flag = 1;
         while (playerPosition < WINNING_POS) {
 
             let optionDiceRoll = Math.floor(Math.random() * 3);
             switch (optionDiceRoll) {
                 case 0:
-                    console.log("No Play..!! Player at Position: " + playerPosition);
+                    console.log("No Play..!! " + player + " at Position: " + playerPosition);
                     break;
                 case 1:
                     let latestDiceRoll = this.rollDice();
@@ -27,19 +31,34 @@ class SnakeAndLadder {
                     if (playerPosition > 100) {
                         playerPosition -= latestDiceRoll;
                     }
-                    console.log("Ladder..!! Player at Position: " + playerPosition);
+                    console.log("Ladder..!! " + player + " at Position: " + playerPosition);
                     break;
                 case 2:
                     playerPosition -= this.rollDice();
                     if (playerPosition < 0) {
                         playerPosition = 0;
                     }
-                    console.log("Snake..!! Player at Position: " + playerPosition);
+                    console.log("Snake..!! " + player + " at Position: " + playerPosition);
                     break;
             }
             console.log("**************************************");
+            flag++;
+            if (playerPosition == 100) {
+                break;
+            }
+            if (flag % 2 == 0) {
+                player = "Player 2";
+                playerOnePosition = playerPosition;
+                playerPosition = playerTwoPosition;
+            }
+            else {
+                player = "Player 1";
+                playerTwoPosition = playerPosition;
+                playerPosition = playerOnePosition;
+            }
         }
-        console.log("Player Wins...!! \nNumber of times dice rolled = " + diceCount);
+        console.log("Number of times dice rolled = " + diceCount + "\n-------------------------------------");
+        console.log("\t  " + player + " Wins...!! \n-------------------------------------");
     }
 }
 
