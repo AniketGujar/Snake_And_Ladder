66d142a2116f7a2e63b0df7b2dbeca2cacf7ff35
AniketGujar <gujaraniket7875@gmail.com>
2021-01-09 18:49:11 +0530

    Added: Player at zeroth position

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("Welcome to the Snake And Ladder Game..!!");
+
+class SnakeAndLadder {
+    Position = () => {
+        let playerPosition = 0;
+        return playerPosition;
+    }
+}
+
+module.exports = new SnakeAndLadder();
---
+++ utility.js
@@ -0,0 +1,3 @@
+let utility = require("./main");
+
+console.log("Player at Position: " + utility.Position());
